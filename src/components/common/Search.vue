4b04998eb8897e8bf004167f862b2b0df55f9eaa
Admin <haoshiliang1@163.com>
2019-06-17 18:46:21 +0800

    去掉添加DTO,优化操作

--- src/components/common/Search.vue
+++ src/components/common/Search.vue
@@ -14,7 +14,7 @@
           <div class="searchTitle">{{field.Title}}</div>
           <span class="searchFlag">：</span>
           <div class="searchInputDiv">
-            <el-select size="small" v-model="field.Value" placeholder="--全部--" class="searchInput" @change="selectChange">
+            <el-select size="small" v-model="field.Value" placeholder="--全部--" class="searchInput" @change="selectChange(field.Value,field.TargetName)">
               <el-option v-for="item in field.BinderList":key="item.Id" :label="item.Name" :value="item.Id"></el-option>
             </el-select>
           </div>
@@ -52,9 +52,31 @@
         opensAdvanced(){
           this.$refs.sAdvanced.openWin();
         },
-        selectChange:function(v) {
-          alert(v);
+        selectChange:function(v,targetName) {
+          this.getSelectBinder(v,targetName);
           this.getList();
+        },
+        getSelectBinder:function (v,targetName) {
+          if (targetName!=""){
+            var whereValue=null;
+            for(var i=0,len=this.queryParam.WhereList.length;i<len;i++){
+              if (this.queryParam.WhereList[i].ParamName===targetName){
+                whereValue = this.queryParam.WhereList[i];
+                break;
+              }
+            }
+            if (whereValue!=null){
+              var binderList=[];
+              whereValue.Value="";
+              for (var i=0,len=whereValue.AllBinderList.length;i<len;i++){
+                if (whereValue.AllBinderList[i].RelationId===v){
+                  binderList.push(whereValue.AllBinderList[i]);
+                }
+              }
+              whereValue.BinderList = binderList;
+              this.getSelectBinder('',whereValue.TargetName);
+            }
+          }
         }
       },
       created: function () {
