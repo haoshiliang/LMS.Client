fcaf24aaa441ffab589c5c53c68a7295b9eeb6d3
Admin <haoshiliang1@163.com>
2019-08-08 15:26:57 +0800

    删除不需求的界面

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,5 +1,63 @@
 <template>
   <div>
-     INDEX页
+    <el-row>
+      <el-col :span="12" style="padding: 6px 3px 6px 6px;">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>标题名称</span>
+          </div>
+          <div v-for="o in 8" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12" style="padding: 6px 6px 6px 3px;">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>标题名称</span>
+          </div>
+          <div v-for="o in 8" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12" style="padding: 6px 3px 6px 6px;">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>标题名称</span>
+          </div>
+          <div v-for="o in 8" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12" style="padding: 6px 6px 6px 3px;">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>标题名称</span>
+          </div>
+          <div v-for="o in 8" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
+<script>
+  export default {
+    name: 'index',
+    data() {
+      return {
+        showLoading:false,
+        isDisabled:false
+      }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+  }
+</script>
